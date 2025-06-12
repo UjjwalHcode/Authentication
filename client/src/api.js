38781d91cbf180621bb90aa1074702b4f0fce736
Ujjwal Mishra <ujjwalmishra714@gmail.com>
2025-06-12 13:39:03 +0530

    Frontend Deployement changes

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
 
 export default api;
-
